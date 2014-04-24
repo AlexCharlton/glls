cb2aabb9f1f614a81f0bd5a1c2ec0ef575936582
Alex Charlton <alex.n.charlton@gmail.com>
2014-04-23 22:01:00 -0400

    Fix related to 7a48dfe

--- glls.scm
+++ glls.scm
@@ -155,14 +155,16 @@
            [attribute-location
             (lambda (a)
               (match-let* ([(name . type) a]
-                           [location (gl:get-attrib-location program
-                                                             (symbol->string name))])
+                           [location (gl:get-attrib-location
+                                      program
+                                      (symbol->string (symbol->glsl name)))])
                           (list name location type)))]
            [uniform-location
             (lambda (u)
               (match-let* ([(name . type) u]
-                           [location (gl:get-uniform-location program
-                                                              (symbol->string name))])
+                           [location (gl:get-uniform-location
+                                      program
+                                      (symbol->string (symbol->glsl name)))])
                           (list name location type)))])
       (for-each (lambda (s)
                   (gl:detach-shader program (shader-program s)))
