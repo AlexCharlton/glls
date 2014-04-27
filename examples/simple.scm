68b21a1afa55b92501a29daeb7a8d44d8271071b
Alex Charlton <alex.n.charlton@gmail.com>
2014-04-27 15:52:10 -0400

    Fix example

--- examples/simple.scm
+++ examples/simple.scm
@@ -3,17 +3,17 @@
 (use glls (prefix glfw3 glfw:) (prefix opengl-glew gl:))
 
 (defpipeline foo 
-  (#:vertex ((vertex #:vec2) (color #:vec3) #:uniform (view-matrix #:mat4))
+  ((#:vertex) ((vertex #:vec2) (color #:vec3) #:uniform (view-matrix #:mat4))
      (define (main) #:void
        (set! gl:position (* view-matrix (vec4 vertex 0.0 1.0)))
        (set! c color))
      -> ((c #:vec3)))
-  (#:fragment ((c #:vec3))
+  ((#:fragment) ((c #:vec3))
      (define (main) #:void
        (set! frag-color (vec4 c 1.0)))
      -> ((frag-color #:vec4))))
 
-(defshader bar #:vertex
+(defshader bar (#:vertex)
     ((vertex #:vec2) (color #:vec3) #:uniform (view-matrix #:mat4))
   (define (main) #:void
     (set! gl:position (* view-matrix (vec4 vertex 0.0 1.0)))
@@ -27,7 +27,7 @@
 (define quox 
   (create-pipeline
    bar
-   '(#:fragment ((c #:vec3))
+   '((#:fragment) ((c #:vec3))
                (define (main) #:void
                  (set! frag-color (vec4 c 1.0)))
                -> ((frag-color #:vec4)))))
